b620844481d9e4b5e2d54daf6c92795770b27bcf
herig <herig_xh@126.com>
2022-03-20 21:45:30 +0800

    6.（必做）写一段代码，使用 HttpClient 或 OkHttp访问http://localhost:8801/

---
+++ src/main/java/week02/job06/HttpSocketServer01.java
@@ -0,0 +1,36 @@
+package week02.job06;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author herig
+ * @date 2022/3/20 - 21:31
+ */
+public class HttpSocketServer01 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while(true){
+            Socket socket = serverSocket.accept();
+            service(socket);
+        }
+    }
+
+    public static void service(Socket socket){
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+            printWriter.println("HTTP/1.0 200 OK");
+            printWriter.println("Content-type:text/html;charset=utf-8");
+            String body = "hello,nio1";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
