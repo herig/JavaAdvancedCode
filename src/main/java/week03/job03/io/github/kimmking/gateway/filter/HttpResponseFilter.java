a1f623b2e7bd06a398335bf7a1885ccc20f93417
herig <herig_xh@126.com>
2022-03-20 23:32:20 +0800

    第三周作业 3.（必做）实现过滤器。SenderHeaderHttpRequestFilter

---
+++ src/main/java/week03/job03/io/github/kimmking/gateway/filter/HttpResponseFilter.java
@@ -0,0 +1,9 @@
+package week03.job03.io.github.kimmking.gateway.filter;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+
+public interface HttpResponseFilter {
+
+    void filter(FullHttpResponse response);
+
+}
